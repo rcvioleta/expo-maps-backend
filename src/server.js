397bbb95b2c2dd8bfe882966d6706a59f5a9de38
Rogene Cris Violeta <rcvioleta13@gmail.com>
2023-02-27 02:11:54 +0800

    IInitial commit w/html string for news entries.

---
+++ src/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+
+const puppeteer = require("puppeteer");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/news-entries", async (req, res, next) => {
+  const browser = await puppeteer.launch({
+    executablePath: "/usr/bin/brave-browser",
+    headless: true,
+  });
+  const page = await browser.newPage();
+
+  await page.goto("https://www.davaocity.gov.ph/tag/road-closures/");
+
+  const element = await page.waitForSelector(".news-entry");
+
+  const htmlString = await element.evaluate((el) => {
+    return el.parentNode.innerHTML;
+  });
+
+  await browser.close();
+
+  res.set("Content-Type", "text/html");
+  res.send(JSON.stringify(htmlString));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
